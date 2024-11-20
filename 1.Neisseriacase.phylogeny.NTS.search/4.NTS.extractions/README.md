4645c1817cbee3526d5989f3b9523dc21e781ed6
lukebharrison <luke.harrison@mail.mcgill.ca>
2024-11-20 14:32:28 -0500

    Initial commit

---
+++ 1.Neisseriacase.phylogeny.NTS.search/4.NTS.extractions/README.md
@@ -0,0 +1,1 @@
+Extraction of NTS and NTS-like/SRM elements from genomes of interest
--- 1.Neisseriacase.phylogeny.NTS.search/4.NTS.extractions/extract.SRM.sh
+++ 1.Neisseriacase.phylogeny.NTS.search/4.NTS.extractions/extract.SRM.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ### Script to search for NTS-like elements and extract them
 
-NTS="CGTCATTCCCGCG[AC]A[ACG]GCGGGAATC[CT][AG]G"
 NTS_LIKE="[CTA]-G-T-C-[AT]-[TC]-N(0,2)-[CT]-[CT]-[CGA]-[CG]-[CTG]-[GA]-[CAT]-N(0,5)-[AT]-N-[GAC]-[CG]-[GCT]-[GA]-G-N(0,2)-[AG]-N-C-[CT]-N-[TCG]"
 SEQS=`fuzznuc -sequence ${1} -pattern $NTS_LIKE -complement -pmismatch $2 -pname NTS_like -stdout -warning N -auto | grep "NTS_like:" | awk '{print $6}'`
 COUNTER=1
